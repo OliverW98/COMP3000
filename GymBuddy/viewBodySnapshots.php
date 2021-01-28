e570ab7c18a987c059dd6001a076e01fc2ea2d61
oliver wilkes <oliverwilkes22@gmail.com>
2021-01-28 12:12:52 +0000

    complete view bod snapshot page

--- GymBuddy/viewBodySnapshots.php
+++ GymBuddy/viewBodySnapshots.php
@@ -11,7 +11,7 @@ if (isset($_POST['btnFindYear'])) {
     if ($_POST['selectYear'] === "Choose a Year...") {
         $failureOutputPara = "Must choose a year";
     } else {
-        $user = getUserWithYear($_SESSION['userID'], $_POST['selectYear']); //Snapshots not return sorted by year
+        $user = getUserWithYear($_SESSION['userID'], $_POST['selectYear']);
         $count = 0;
         foreach ($user->getSnapshots() as $snapshots) {
             $count++;
@@ -23,15 +23,7 @@ if (isset($_POST['btnFindYear'])) {
             $snapshotWeights = getSnapshotWeights($bodySnapshots);
             $snapshotBFPs = getSnapshotBFP($bodySnapshots);
             $snapshotMMPs = getSnapshotMMP($bodySnapshots);
-//            $cyclesAMonth = getCyclesAMonth($cycleWorkouts);
-//            $totalDis = getTotalDistance($cycleWorkouts);
-//            $totalDurMins = getTotalDuration($cycleWorkouts);
-//            $avDis = getAverageDistance($cycleWorkouts);
-//            $avDur = getAverageDuration($cycleWorkouts);
-//            $avSpeed = getAverageSpeed($cycleWorkouts);
-//            $avWatts = getAverageWatts($cycleWorkouts);
-//            $avCals = getAverageCals($cycleWorkouts);
-//            $totalCycles = count($cycleWorkouts);
+            $currentSnapshotWeights = getCurrentSnapshotWeights($bodySnapshots[0]);
         } else {
             $failureOutputPara = "No Body Snapshots recorded for " . $_POST['selectYear'];
         }
@@ -86,74 +78,16 @@ function getSnapshotMMP($bodySnapshots)
     return $snapshotMMP;
 }
 
-//// count how many rides a month
-//function getCyclesAMonth($cycleWorkouts)
-//{
-//    $cyclesAMonth = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-//    for ($i = 0; $i < count($cycleWorkouts); $i++) {
-//        for ($j = 1; $j <= 12; $j++) {
-//            if (substr($cycleWorkouts[$i]->getDate(), 5, 2) == strval($j)) {
-//                $cyclesAMonth[$j - 1] = $cyclesAMonth[$j - 1] + 1;
-//            }
-//        }
-//    }
-//    return $cyclesAMonth;
-//}
-//
-//function getTotalDistance($cycleWorkouts)
-//{
-//    $totalDis = 0;
-//    foreach ($cycleWorkouts as $cycle) {
-//        $totalDis = $totalDis + $cycle->getDistance();
-//    }
-//    return $totalDis;
-//}
-//
-//function getTotalDuration($cycleWorkouts)
-//{
-//    $totalDur = 0;
-//    foreach ($cycleWorkouts as $cycle) {
-//        $totalDur = $totalDur + $cycle->getDuration();
-//    }
-//    return $totalDur;
-//}
-//
-//function getAverageDistance($cycleWorkouts)
-//{
-//    return getTotalDistance($cycleWorkouts) / count($cycleWorkouts);
-//}
-//
-//function getAverageDuration($cycleWorkouts)
-//{
-//    return getTotalDuration($cycleWorkouts) / count($cycleWorkouts);
-//}
-//
-//function getAverageSpeed($cycleWorkouts)
-//{
-//    $totalSpeed = 0;
-//    foreach ($cycleWorkouts as $cycle) {
-//        $totalSpeed = $totalSpeed + $cycle->getSpeed();
-//    }
-//    return $totalSpeed / count($cycleWorkouts);
-//}
-//
-//function getAverageWatts($cycleWorkouts)
-//{
-//    $totalWatts = 0;
-//    foreach ($cycleWorkouts as $cycle) {
-//        $totalWatts = $totalWatts + $cycle->getAverageWatts();
-//    }
-//    return $totalWatts / count($cycleWorkouts);
-//}
-//
-//function getAverageCals($cycleWorkouts)
-//{
-//    $totalCals = 0;
-//    foreach ($cycleWorkouts as $cycle) {
-//        $totalCals = $totalCals + $cycle->getCaloriesBurnt();
-//    }
-//    return $totalCals / count($cycleWorkouts);
-//}
+function getCurrentSnapshotWeights($snapshot)
+{
+    $Weights = array();
+    $fatWeight = round(($snapshot->getBodyFatPercent() / 100) * $snapshot->getWeight());
+    $muscleWeight = round(($snapshot->getMuscleMassPercent() / 100) * $snapshot->getWeight());
+    array_push($Weights, $snapshot->getWeight() - ($fatWeight + $muscleWeight));
+    array_push($Weights, $fatWeight);
+    array_push($Weights, $muscleWeight);
+    return $Weights;
+}
 
 
 ?>
@@ -180,28 +114,10 @@ function getSnapshotMMP($bodySnapshots)
         </div>
     </form>
     <p class="text-center text-danger"><?php echo $failureOutputPara ?></p>
-    <canvas id="averageSpeedChart" width="200" height=75"></canvas>
-    <!--    <canvas id="distanceRiddenChart" width="200" height="75"></canvas>-->
-    <!--    <canvas id="averageWattsChart" width="200" height="75"></canvas>-->
-    <!--    <canvas id="RidePerMonth" width="200" height=100"></canvas>-->
-    <!--    <div class="row">-->
-    <!--        <div class="col-sm-6 mt-5">-->
-    <!--            <h4 class="text-center">Year Total</h4>-->
-    <!--            <P>Number of Rides : --><?php //echo $totalCycles ?><!--</P>-->
-    <!--            <p>Distance : --><?php //echo round($totalDis / 1000, 1) ?><!-- Km</p>-->
-    <!--            <P>Duration-->
-    <!--                : -->
-    <?php //echo $totalDurHrs = floor($totalDurMins / 60) . 'h' . ($totalDurMins - floor($totalDurMins / 60) * 60 . 'm'); ?><!--</P>-->
-    <!--        </div>-->
-    <!--        <div class="col-sm-6 mt-5">-->
-    <!--            <h4 class="text-center">Average Ride</h4>-->
-    <!--            <p>Distance : --><?php //echo round($avDis / 1000, 1) ?><!-- Km</p>-->
-    <!--            <P>Duration : --><?php //echo $avDur ?><!-- Mins</P>-->
-    <!--            <P>Speed : --><?php //echo round($avSpeed * 3.6, 1) ?><!-- Km/h</P>-->
-    <!--            <P>Watts : --><?php //echo round($avWatts, 1) ?><!--</P>-->
-    <!--            <P>Calories Burnt : --><?php //echo round($avCals) ?><!--</P>-->
-    <!--        </div>-->
-    <!--    </div>-->
+    <h4 class="text-center">Current Body Composition</h4>
+    <canvas id="CurrentBodySnapshotChart" width="200" height="75"></canvas>
+    <h4 class="text-center mt-5">Past Snapshots</h4>
+    <canvas id="bodySnapshotChart" width="200" height=75"></canvas>
 </div>
 </body>
 </html>
@@ -219,14 +135,16 @@ function getSnapshotMMP($bodySnapshots)
     $js_array = json_encode($snapshotMMPs);
     echo "let snapshotMMPs = " . $js_array . ";\n";
 
+    $js_array = json_encode($currentSnapshotWeights);
+    echo "let currentSnapshot = " . $js_array . ";\n";
+
     $js_array = json_encode($snapshotDates);
     echo "let snapshotDates = " . $js_array . ";\n";
 
     ?>
 
 
-
-    var ctx = document.getElementById('averageSpeedChart').getContext('2d');
+    var ctx = document.getElementById('bodySnapshotChart').getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
@@ -262,79 +180,20 @@ function getSnapshotMMP($bodySnapshots)
         }
     });
 
-    // var ctx1 = document.getElementById('distanceRiddenChart').getContext('2d');
-    // var myChart = new Chart(ctx1, {
-    //     type: 'line',
-    //     data: {
-    //         labels: cycleDates,
-    //         datasets: [{
-    //             label: 'Distance Riden (Km)',
-    //             data: distanceRidden,
-    //             fill: false,
-    //             borderColor: 'navy',
-    //             borderWidth: 2
-    //         }]
-    //     },
-    //     options: {
-    //         scales: {
-    //             yAxes: [{
-    //                 ticks: {
-    //                     beginAtZero: true
-    //                 }
-    //             }]
-    //         }
-    //     }
-    // });
-    //
-    // var ctx2 = document.getElementById('averageWattsChart').getContext('2d');
-    // var myChart = new Chart(ctx2, {
-    //     type: 'line',
-    //     data: {
-    //         labels: cycleDates,
-    //         datasets: [{
-    //             label: 'Average Watts',
-    //             data: averageWatts,
-    //             fill: false,
-    //             borderColor: 'navy',
-    //             borderWidth: 2
-    //         }]
-    //     },
-    //     options: {
-    //         scales: {
-    //             yAxes: [{
-    //                 ticks: {
-    //                     beginAtZero: true
-    //                 }
-    //             }]
-    //         }
-    //     }
-    // });
 
-    // var ctx3 = document.getElementById('RidePerMonth').getContext('2d');
-    // var RidePerMonth = new Chart(ctx3, {
-    //     type: 'bar',
-    //     data: {
-    //         labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    //         datasets: [{
-    //             label: 'Rides a Month',
-    //             data: cyclesAMonth,
-    //             backgroundColor: 'navy',
-    //             borderColor: 'navy',
-    //             borderWidth: 1
-    //         }]
-    //     },
-    //     options: {
-    //         scales: {
-    //             yAxes: [{
-    //                 ticks: {
-    //                     beginAtZero: true,
-    //                     stepSize: 1
-    //                 }
-    //             }]
-    //         }
-    //     }
-    // });
-
+    var ctx1 = document.getElementById('CurrentBodySnapshotChart').getContext('2d');
+    var myChart = new Chart(ctx1, {
+        type: 'pie',
+        data: {
+            labels: ["Weight (Kg)", "Body Fat (Kg)", "Muscle Mass (Kg)"],
+            datasets: [{
+                label: 'Weight (Kg)',
+                data: currentSnapshot,
+                backgroundColor: ['navy', 'red', 'green']
+            }]
+        },
+        options: {}
+    });
 
 </script>
 
